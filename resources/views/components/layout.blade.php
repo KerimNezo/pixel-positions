1fd4394020f1f87c806b59d367c3d237a827f9ea
Kerim Nezo <kerim.nezo@gmail.com>
2024-06-08 23:57:36 +0200

    Added css to layout and welcome blade components
    Added css to buttons, links and cards on the layout and welcome blades.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,36 +4,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body style="background-color: black">
+<body style="background-color: #060606; margin: 0px; color: white;  ">
 
-    <div>
-        <nav style="display: flex;
-    align-items: center;
-    justify-content: space-between;">
-            <div style="display: flex;
-    align-items: center;">
-                <a href="" style="margin-left: 15px;">
+    <div style="padding-left: 25px; padding-right: 25px;">
+        <nav style="display: flex; align-items: center; justify-content: space-between; padding-top: 10px; padding-bottom: 10px; border-bottom: 1px solid #888888;">
+            <div style="display: flex; align-items: center;">
+                <a href="" style="">
                     <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
                 </a>
             </div>
 
-            <div style="display: flex;
-    align-items: center;">
-                <a href="#" style="margin-left: 15px;">Jobs</a>
-                <a href="#" style="margin-left: 15px;">Carrers</a>
-                <a href="#" style="margin-left: 15px;">Salaries</a>
-                <a href="#" style="margin-left: 15px;">Companies</a>
+            <div style="display: flex; align-items: center; text-color: white; font-weight: bold;">
+                <a href="#" style="margin-left: 0px; color: white; text-decoration: none;">Jobs</a>
+                <a href="#" style="margin-left: 15px; color: white; text-decoration: none;">Carrers</a>
+                <a href="#" style="margin-left: 15px; color: white; text-decoration: none;">Salaries</a>
+                <a href="#" style="margin-left: 15px; color: white; text-decoration: none;">Companies</a>
             </div>
 
-            <div style="display: flex;
-    align-items: center;">
-                <a href="#" style="margin-left: 15px;">Post a job</a>
+            <div style="display: flex; align-items: center;">
+                <a href="#" style="margin-left: 0px; color: white; text-decoration: none;">Post a job</a>
             </div>
         </nav>
 
-        <main>
+        <main style="margin-top: 25px; max-width:986px; ">
             {{ $slot }}
         </main>
     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,24 @@
 <x-layout>
-    Hello world
+    <section>
+        <h3 style="margin-bottom: 15px;"> Top Jobs </h3>
+
+        <div style="padding: 15px; background-color:coral; border-radius: 15px; display: flex; flex-direction:column; text-align: center; ">
+            <div style="text-align: start; font-size: 14px;">Laracasts</div>
+
+            <div style="padding-bottom: 20px; padding-top: 20px; font-weight: bold;">
+                <h3> Video editor </h3>
+                <p> Full time - From $60,000 </p>
+            </div>
+
+            <div style="display: flex; justify-content: space-between; align-items: center; margin-top: auto; ">
+                <div>
+                    <a href="#" style="background-color: #c5baba; padding: 5px 10px 5px 10px; border-radius: 15px; font-size: 12px; text-decoration: none;">Tag</a>
+                    <a href="#">Tag</a>
+                    <a href="#">Tag</a>
+                </div>
+
+                <img src="https://placehold.it/42/42" alt="">
+            </div>
+        </div>
+    </section>
 </x-layout>
