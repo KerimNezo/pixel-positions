99a30219fd21d6184e1059263e13a9c98b77533a
Kerim Nezo <kerim.nezo@gmail.com>
2024-09-11 15:04:32 +0200

    Edited employer logo and added laraveldebugbar package
    As the summary says

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,5 +1,5 @@
 @props(['employer', 'width' => 90])
 
 @if ($employer->hasMedia('logos'))
-    <img src="{{ asset($employer->getFirstMediaUrl('logos')) }}" alt="Employer Logo" style="border-radius: 15px;" width="{{ $width }}px" height="{{ $width }}px">
+    <img src="{{ $employer->getFirstMediaUrl('logos') }}" alt="Employer Logo" style="border-radius: 15px;" width="{{ $width }}px" height="{{ $width }}px">
 @endif
